2950fdad6efd68409f1323f64935afaa01335aa2
Raymond Thurman <raymondthurman5@gmail.com>
2020-09-23 20:54:05 -0400

    ✨ updated mult tables to go up to 15

--- server/routes/math/index.js
+++ server/routes/math/index.js
@@ -2,7 +2,7 @@ const mathRouter = require('express').Router();
 const { generateQuestions, shuffleArray } = require('../../utils');
 
 mathRouter.get('/multiplication-tables', (req, res) => {
-  let questions = generateQuestions(Math.floor(Math.random() * 10) + 1);
+  let questions = generateQuestions(Math.floor(Math.random() * 15) + 1);
   const shuffledQuestions = shuffleArray(questions);
   res.json(shuffledQuestions);
 });
