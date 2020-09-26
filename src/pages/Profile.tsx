19184526b5870fbdf6b247a897ed4cc2b372db31
Raymond Thurman <raymondthurman5@gmail.com>
2020-09-26 11:07:29 -0400

    🐛 minor image optimization and typo fixes

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Wrapper } from './Dashboard';
 
-const Profile = () => {
+const Profile = ({ welcomeMessage }: any) => {
   return (
     <Wrapper>
-      <p>Profile</p>
+      <p>{welcomeMessage}</p>
       <p>This page is currently under development.</p>
-      <p>Check back shortly to see the games dashboard!</p>
+      <p>Check back shortly to see your user profile!</p>
+      <p>If you are a parent, your childs progress will be here as well</p>
     </Wrapper>
   );
 };
 
-export default Profile;
+const mapStateToProps = (state: any) => ({
+  welcomeMessage: state.user.message,
+});
+
+export default connect(mapStateToProps, null)(Profile);
