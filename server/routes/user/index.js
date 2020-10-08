8dce2c572ac56d1acf49d33e372f0b65863707d3
Raymond Thurman <raymondthurman5@gmail.com>
2020-10-07 22:43:43 -0400

    ✨ initial route to get user tests

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -2,7 +2,7 @@ const userRouter = require('express').Router();
 const { User } = require('../../models');
 
 userRouter
-  .post('/child', async (req, res) => {
+  .post('/child', async (req, res, next) => {
     try {
       const children = await User.findAll({
         parent_email: req.body.email,
@@ -13,10 +13,18 @@ userRouter
     }
   })
   // Temp route to keep track of my users for setting everything up
-  .get('/users', async (req, res) => {
+  .get('/users', async (req, res, next) => {
     try {
       const users = await User.find();
       return res.status(200).json(users);
+    } catch (err) {
+      next(err);
+    }
+  })
+  .get('/user-tests/:id', async (req, res, next) => {
+    try {
+      const tests = await User.findAll({ user_id: req.params.id });
+      return res.status(200).json(tests);
     } catch (err) {
       next(err);
     }
