67c570761cca20d3e6d6b6cfe504082646cdb9bf
Raymond Thurman <raymondthurman5@gmail.com>
2020-10-07 23:08:46 -0400

    initial tests table complete

--- server/data/migrations/20201007222745_tests.js
+++ server/data/migrations/20201007222745_tests.js
@@ -1,8 +1,11 @@
 exports.up = async function (knex) {
   await knex.schema.createTable('tests', tbl => {
     tbl.increments('id');
-    tbl.integer('user_id');
+    tbl.integer('user_id').notNullable();
     tbl.foreign('user_id').references('users.id');
+    tbl.integer('score');
+    tbl.string('subject', 128);
+    tbl.timestamp('created_at').defaultTo(knex.fn.now());
     // Test will need questions
     // Foreign key that will
     // Reference a user id
